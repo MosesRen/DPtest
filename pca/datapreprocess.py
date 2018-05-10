f9ec888c1c6bb7b94cb20de06acd61349e6e9e9e
mosesren <renhui774411@163.com>
2018-05-10 22:41:13 +0800

    5.10update add random-forest

--- pca/datapreprocess.py
+++ pca/datapreprocess.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+__author__ = "mosesren"
 import numpy as np 
 import pandas as pd
 from sklearn.model_selection import train_test_split
@@ -12,8 +14,7 @@ def load_traindata(filepath):
 
 	csv = pd.read_csv(filepath,index_col = None,parse_dates = True,iterator = True)
 	dataset = csv.read()
-
-	# # 读取数据集的某些列
+	# # 读取数据集
 	return dataset
 def OneHot(dataset):
 	#对分类属性进行onehot编码
@@ -39,29 +40,32 @@ def adult_removenull(filepath):
 	#输出处理后的文件
 	return c
 if __name__ == '__main__':
-	dataset = load_traindata(r'./dataset/adlutNoNull.csv')
-	train_data = OneHot(dataset).values
+	dataset = load_traindata(r'../dataset/adlutNoNull.csv')
+	
+	# train_data = OneHot(dataset).values
+	train_data = dataset.iloc[:,[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]].values
+	print(train_data[1])
 	#onehot编码
 	train_target = dataset.loc[:,['class']].values
 	# # 读取标签列
-	train_data = normalize(train_data)
+	# train_data = normalize(train_data)
 	#数据归一化
-	train_data,recov= pcacn(train_data, 12)
+	# train_data,recov= pcacn(train_data, 12)
 	#主成分分析
-	X_train, X_test, y_train, y_test =  train_test_split(recov, train_target,test_size=0.3, random_state=42)
-	#分割训练集测试集
-	from sklearn.svm import SVC
-	#SVM训练
-	svm_model = SVC(gamma='auto')
-	svm_fit_model = svm_model.fit(X_train,y_train.ravel())
-	y_pred_svm = svm_model.predict(X_test)
-	y_true_svm = y_test.ravel()
-	print('Accuracy = ', accuracy_score(y_true_svm, y_pred_svm))
+	X_train, X_test, y_train, y_test =  train_test_split(train_data, train_target,test_size=0.3, random_state=42)
+	# 分割训练集测试集
+	# from sklearn.svm import SVC
+	# #SVM训练
+	# svm_model = SVC(gamma='auto')
+	# svm_fit_model = svm_model.fit(X_train,y_train.ravel())
+	# y_pred_svm = svm_model.predict(X_test)
+	# y_true_svm = y_test.ravel()
+	# print('Accuracy = ', accuracy_score(y_true_svm, y_pred_svm))
 
 	# from sklearn.ensemble import RandomForestClassifier
 	# #随机森林训练
 	# rf_model =  RandomForestClassifier(n_estimators=15, n_jobs=8)
-	# rf_fit_model = rf_model.fit(X_train, y_train.ravel()) 
+	# rf_fit_model = rf_model.fit(X_train, y_train.ravel())
 	# y_pred_rf = rf_model.predict(X_test)
 	# y_true_rf = y_test.ravel()
 	# print('Accuracy = ', accuracy_score(y_true_rf, y_pred_rf))
